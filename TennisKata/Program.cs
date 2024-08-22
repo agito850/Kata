f1349db47d12b57c7c25bb89a224d60a06617dc7
Shon Pan <agito850@gmail.com>
2024-08-22 22:14:35 +0800

    [Feat] 新增專案 完成項目Love-All

---
+++ TennisKata/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using TennisKata;
+
+Main();
+return;
+
+static void Main()
+{
+    var tennis = new Tennis("Sam1", "Sam2");
+
+    var score = tennis.Score();
+    Console.WriteLine(score);
+}
---
+++ TennisKata/TennisTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace TennisKata;
+
+[TestFixture]
+[TestOf(typeof(Tennis))]
+public class TennisTest
+{
+
+    [Test]
+    public void Love_All()
+    {
+        var tennis = new Tennis("Sam1", "Sam2");
+        var score = tennis.Score();
+        Assert.AreEqual("Love-All", score);
+    }
+}
