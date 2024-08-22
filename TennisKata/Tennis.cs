d696bdccf753e8499c8701b56c131ce36c4c878b
Shon Pan <agito850@gmail.com>
2024-08-22 22:38:49 +0800

    [Feat] 新增Dictionary及各分數UnitTest

--- TennisKata/Tennis.cs
+++ TennisKata/Tennis.cs
@@ -7,6 +7,14 @@ public class Tennis
     private int _playerTwoScore;
     private readonly string _playerTwoName;
 
+    private Dictionary<int, string> scoreDic = new Dictionary<int, string>()
+    {
+        { 0, "Love" }, 
+        { 1, "Fifteen" }, 
+        { 2, "Thirty" }, 
+        { 3, "Forty" }
+    };
+
     public Tennis(string playerOneName, string playerTwoName)
     {
         _playerOneName = playerOneName;
@@ -14,24 +22,36 @@ public class Tennis
         _playerOneScore = 0;
         _playerTwoScore = 0;
     }
+
     public string Score()
     {
         //1. 輸出必須為網球分數 例：1:0 => Fifteen Love 
         //2. 分數表達必須是文字例如Fifteen Forty 而非15：40 
         //3. 賽末點時 輸出為 Player Name Adv, 例：Sam Adv
         //4. 勝出時 輸出為 Player Name Win, 例：Sam Win
-        //return "Love All";
-        return "Love-All";
+        if(_playerOneScore == _playerTwoScore)
+        {
+            return _playerOneScore switch
+            {
+                0 => "Love All",
+                1 => "Fifteen All",
+                2 => "Thirty All",
+                3 => "Deuce",
+                _ => "??"
+            };
+        }
+        var playerOneScore = scoreDic[_playerOneScore];
+        var playerTwoScore = scoreDic[_playerTwoScore];
+        return playerOneScore + " " + playerTwoScore;
     }
 
     public void PlayerOneScore()
     {
         _playerOneScore++;
     }
-    
+
     public void PlayerTwoScore()
     {
-        _playerTwoScore++; 
+        _playerTwoScore++;
     }
-
 }
--- TennisKata/TennisTest.cs
+++ TennisKata/TennisTest.cs
@@ -6,12 +6,102 @@ namespace TennisKata;
 [TestOf(typeof(Tennis))]
 public class TennisTest
 {
+    private Tennis _tennis;
+    
+    [SetUp]
+    public void SetUp()
+    {
+        _tennis = new Tennis("Sam Adv", "Sam Win");
+    }
+    
+    private void ScoreShouldBe(string expected)
+    {
+        var score = _tennis.Score();
+        Assert.AreEqual(expected, score);
+    }
 
     [Test]
     public void Love_All()
     {
-        var tennis = new Tennis("Sam1", "Sam2");
-        var score = tennis.Score();
-        Assert.AreEqual("Love-All", score);
+        ScoreShouldBe("Love All");
     }
+    
+    [Test]
+    public void Fifteen_Love()
+    {
+        _tennis.PlayerOneScore();
+        ScoreShouldBe("Fifteen Love");
+    }
+    
+    [Test]
+    public void Thirty_Love()
+    {
+        _tennis.PlayerOneScore();
+        _tennis.PlayerOneScore();
+        ScoreShouldBe("Thirty Love");
+    }
+    
+    [Test]
+    public void Forty_Love()
+    {
+        _tennis.PlayerOneScore();
+        _tennis.PlayerOneScore();
+        _tennis.PlayerOneScore();
+        ScoreShouldBe("Forty Love");
+    }
+    
+    [Test]
+    public void Love_Fifteen()
+    {
+        _tennis.PlayerTwoScore();
+        ScoreShouldBe("Love Fifteen");
+    }
+    
+    [Test]
+    public void Love_Thirty()
+    {
+        _tennis.PlayerTwoScore();
+        _tennis.PlayerTwoScore();
+        ScoreShouldBe("Love Thirty");
+    }
+    
+    [Test]
+    public void Love_Forty()
+    {
+        _tennis.PlayerTwoScore();
+        _tennis.PlayerTwoScore();
+        _tennis.PlayerTwoScore();
+        ScoreShouldBe("Love Forty");
+    }
+    
+    [Test]
+    public void Fifteen_All()
+    {
+        _tennis.PlayerOneScore();
+        _tennis.PlayerTwoScore();
+        ScoreShouldBe("Fifteen All");
+    }
+    
+    [Test]
+    public void Thirty_All()
+    {
+        _tennis.PlayerOneScore();
+        _tennis.PlayerOneScore();
+        _tennis.PlayerTwoScore();
+        _tennis.PlayerTwoScore();
+        ScoreShouldBe("Thirty All");
+    }
+    
+    [Test]
+    public void Deuce()
+    {
+        _tennis.PlayerOneScore();
+        _tennis.PlayerOneScore();
+        _tennis.PlayerOneScore();
+        _tennis.PlayerTwoScore();
+        _tennis.PlayerTwoScore();
+        _tennis.PlayerTwoScore();
+        ScoreShouldBe("Deuce");
+    }
+    
 }
